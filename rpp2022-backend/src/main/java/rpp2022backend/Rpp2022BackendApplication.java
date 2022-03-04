658d04c9fea4436309fddd624988257bd5917772
Miroslav Stefanović <mstef@uns.ac.rs>
2022-03-04 17:56:58 +0100

    Inicijalni projekat

---
+++ rpp2022-backend/src/main/java/rpp2022backend/Rpp2022BackendApplication.java
@@ -0,0 +1,13 @@
+package rpp2022backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rpp2022BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rpp2022BackendApplication.class, args);
+	}
+
+}
